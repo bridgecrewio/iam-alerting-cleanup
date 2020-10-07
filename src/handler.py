67303632ffd70a6ee7ce8e646648d5a6b4cbf083
Mike Urbanski <mike@bridgecrew.io>
2020-10-07 18:02:03 -0500

    add logger

--- src/handler.py
+++ src/handler.py
@@ -1,6 +1,13 @@
+import os
+import sys
+
 import boto3
 from boto3_type_annotations.s3 import Client as S3Client
+import logging
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(os.environ.get('LOG_LEVEL', 'INFO'))
 
 def handler(event, context):
     client: S3Client = boto3.client('s3', region_name='us-east-1')
-    print(client.list_buckets())
+    LOGGER.info(client.list_buckets())
